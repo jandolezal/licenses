2e7344c199c9dfa1df12e68055e3e9115a55ef92
Jan Doležal <jandolezal@email.cz>
2021-07-11 10:33:28 +0200

    Reformat holders spider with Black

--- licenses/spiders/holders.py
+++ licenses/spiders/holders.py
@@ -5,10 +5,10 @@ from licenses.items import HolderItem
 
 
 class HoldersSpider(scrapy.Spider):
-    name = 'holders'
+    name = "holders"
 
     start_urls = [
-        'https://www.eru.cz/licence/informace-o-drzitelich',
+        "https://www.eru.cz/licence/informace-o-drzitelich",
     ]
 
     def parse(self, response):
@@ -20,27 +20,27 @@ class HoldersSpider(scrapy.Spider):
         xml_urls = []
         xml_paras = response.xpath('//p[contains(text(), "XML")]/a/@href')
         for xml_href in xml_paras:
-            xml_urls.append('https://www.eru.cz' + xml_href.get())
+            xml_urls.append("https://www.eru.cz" + xml_href.get())
 
         for url in xml_urls:
-            yield scrapy.Request(url=url, callback=self.parse_xml, encoding='utf-8')
+            yield scrapy.Request(url=url, callback=self.parse_xml, encoding="utf-8")
 
     def parse_xml(self, response):
-        """Parse xml file with actual data about license holders.
-        """
+        """Parse xml file with actual data about license holders."""
 
-        data_list = response.xpath('*')
+        data_list = response.xpath("*")
 
         # Předmět podnikání z názvu souboru, např. 11 výroba elektřiny
-        start = int(response.url.index('LIC_')) + 4
+        start = int(response.url.index("LIC_")) + 4
         end = start + 2
         predmet = response.url[start:end]
 
         # Pomocné funkce pro čistění vstupních dat
         def remove_fluff(x):
-            if (x == '') or ('----' in x):
+            if (x == "") or ("----" in x):
                 return None
             return x
+
         convert_to_int = lambda x: int(x) if x else None
         convert_to_date = lambda x: date.fromisoformat(x) if x else None
         clean_person = lambda x: x if x else None
@@ -50,25 +50,25 @@ class HoldersSpider(scrapy.Spider):
             data_dict = data.attrib
 
             holder = HolderItem(
-                id=data_dict['cislo_licence'],
-                version=convert_to_int(data_dict['version']),
-                status=data_dict['version_status'],
-                ic=remove_fluff(data_dict['subjekt_IC']),
-                nazev=remove_fluff(data_dict['subjekt_nazev']),
-                cislo_dom=remove_fluff(data_dict['subjekt_cislo_dom']),
-                cislo_or=remove_fluff(data_dict['subjekt_cislo_or']),
-                ulice=remove_fluff(data_dict['subjekt_ulice_nazev']),
-                obec_cast=remove_fluff(data_dict['subjekt_obec_cast']),
-                obec=remove_fluff(data_dict['subjekt_obec_nazev']),
-                psc=remove_fluff(data_dict['subjekt_PSC']),
-                okres=remove_fluff(data_dict['subjekt_okres']),
-                kraj=remove_fluff(data_dict['subjekt_kraj']),
-                zeme=remove_fluff(data_dict['subjekt_zeme']),
-                den_opravneni=convert_to_date(data_dict['subjekt_den_opravneni']),
-                den_zahajeni=convert_to_date(data_dict['subjekt_den_zahajeni']),
-                den_zaniku=convert_to_date(data_dict['subjekt_den_zaniku']),
-                den_nabyti=convert_to_date(data_dict['subjekt_den_nabyti_pravni_moci']),
-                osoba=clean_person(data_dict['odpovedny_zast']),
+                id=data_dict["cislo_licence"],
+                version=convert_to_int(data_dict["version"]),
+                status=data_dict["version_status"],
+                ic=remove_fluff(data_dict["subjekt_IC"]),
+                nazev=remove_fluff(data_dict["subjekt_nazev"]),
+                cislo_dom=remove_fluff(data_dict["subjekt_cislo_dom"]),
+                cislo_or=remove_fluff(data_dict["subjekt_cislo_or"]),
+                ulice=remove_fluff(data_dict["subjekt_ulice_nazev"]),
+                obec_cast=remove_fluff(data_dict["subjekt_obec_cast"]),
+                obec=remove_fluff(data_dict["subjekt_obec_nazev"]),
+                psc=remove_fluff(data_dict["subjekt_PSC"]),
+                okres=remove_fluff(data_dict["subjekt_okres"]),
+                kraj=remove_fluff(data_dict["subjekt_kraj"]),
+                zeme=remove_fluff(data_dict["subjekt_zeme"]),
+                den_opravneni=convert_to_date(data_dict["subjekt_den_opravneni"]),
+                den_zahajeni=convert_to_date(data_dict["subjekt_den_zahajeni"]),
+                den_zaniku=convert_to_date(data_dict["subjekt_den_zaniku"]),
+                den_nabyti=convert_to_date(data_dict["subjekt_den_nabyti_pravni_moci"]),
+                osoba=clean_person(data_dict["odpovedny_zast"]),
                 predmet=predmet,
             )
 
